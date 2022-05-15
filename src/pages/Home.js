29b457d6c433d0ac8d1153c8ca3f2a2b167777b4
FibonaccciYan <1363650301@qq.com>
2022-05-15 16:17:38 +0800

    update SignUp with api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,30 @@
 import React from "react";
 //import axios from "axios";
 
-export default function Home(){
-    const SearchResultDisplay = () => { 
-            //  async () => {
-            //      const {data} = await axios.get("http://localhost:3000/people");
-            //      console.log(data);
-            //  }
-        const temp = async () => {
-            const {data} = await axios.get("http://localhost:3000/people");
-            console.log(data);
-        }
-        console.log(2);
+export default function Home() {
+    const SearchResultDisplay = () => {
+        //  async () => {
+        //      const {data} = await axios.get("http://localhost:3000/people");
+        //      console.log(data);
+        //  }
+        // const temp = async () => {
+        //     const {data} = await axios.get("http://localhost:3000/people");
+        //     console.log(data);
+        // }
+        // console.log(2);
     }
-    
-    return(
+
+    return (
         <div
             style={{
-                textAlign:'center',
-                display:"flex",
-                justifyContent:'center',
-                alignItems:'center',
-                height:'90vh',
-                fontSize:'30px',
-                textShadow:'1px 0 5px black,-1px 0 0 black,0 1px 5px black,0 -1px 0 black',
-                color:'white',
+                textAlign: 'center',
+                display: "flex",
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '90vh',
+                fontSize: '30px',
+                textShadow: '1px 0 5px black,-1px 0 0 black,0 1px 5px black,0 -1px 0 black',
+                color: 'white',
             }}
         >
             <h1>
@@ -32,7 +32,7 @@ export default function Home(){
                 <br></br>
                 Demo
             </h1>
-           {/* <div>
+            {/* <div>
                <button type="button" onClick={SearchResultDisplay}>查看</button>
            </div> */}
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,22 +9,22 @@ export default class Login extends React.Component {
             toSignUp: false
         };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
 
-    handleChange(event) {
+    onChange(event) {
         this.setState({ name: event.target.value });
     }
 
-    handleSubmit(event) {
+    onSubmit(event) {
         event.preventDefault();
         alert("用户名：" + this.state.name + "\n登录成功！");
         window.location.href = "/";
     }
 
-    handleClick(event) {
+    onClick(event) {
         event.preventDefault();
         window.location.href = "/signup";
 
@@ -42,20 +42,22 @@ export default class Login extends React.Component {
                     fontSize: '18px',
                 }}
             >
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.onSubmit}>
                     <table>
-                        <tr>
-                            <td>用户名</td>
-                            <td><input type="text" value={this.state.value} onChange={this.handleChange} /></td>
-                        </tr>
-                        <tr>
-                            <td>密码</td>
-                            <td><input type="password" /></td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>用户名</td>
+                                <td><input type="text" value={this.state.value} onChange={this.onChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>密码</td>
+                                <td><input type="password" /></td>
+                            </tr>
+                        </tbody>
                     </table>
                     <br />
                     <input type="submit" value="登录" />
-                    <input type="button" value="注册" onClick={this.handleClick} />
+                    <input type="button" value="注册" onClick={this.onClick} />
                 </form>
             </div>
         )
