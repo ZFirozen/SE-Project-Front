b5d13f88fac5fe6755cd8b3d6977347acb157596
junzhi-cn <80112517+junzhi-cn@users.noreply.github.com>
2022-06-27 13:00:20 +0800

    Revert " class改成const，新增JS012"

--- src/services/ant-design-pro/api.js
+++ src/services/ant-design-pro/api.js
@@ -33,10 +33,10 @@ export async function login(body, options) {
 /** 此处后端没有提供注释 GET /api/notices */
 
 export async function getNotices(options) {
-  return request('/api/notices', {
-    method: 'GET',
-    ...(options || {}),
-  });
+  // return request('/api/notices', {
+  //   method: 'GET',
+  //   ...(options || {}),
+  // });
 }
 /** 获取规则列表 GET /api/rule */
 
